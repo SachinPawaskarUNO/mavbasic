6b92d39acc613924e1f4079e46d33107b3e31dfb
Sachin Pawaskar <spawaskar@unomaha.edu>
2016-11-23 14:55:32 -0600

    Updated Welcome page title and message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,18 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome to Basic Starter Application</div>
+                <div class="panel-heading"><b>Welcome to Basic Starter Application</b></div>
                 <div class="app-welcome"><img src="images/UNO-Mav.png"></div>
                 <div class="panel-body">
-                    Place some welcome text here for the your Basic Starter Application and maybe a description about intended use.
+                    <p>The original intent of this web application is to provide students in the MIS Capstone course with a basic starter web application.
+                        Of course, this can also be used by students in other courses that can benefit from having a basic web application and not have to start from scratch.</p>
+                    <p>
+                        The UNO - MavBasic web application attempts to take the pain out of development by providing common basic functionality such as User Management (includes users, roles and permisssions), Authentication, Authorization, User Settings, etc.
+                        so that students can focus on delivering application functionality focused on meeting customer's business requirements.
+                    </p>
+                    <p>
+                        Documentation for the MavBasic application can be found at <a href="https://SachinPawaskarUNO.github.io/mavbasic" target="_blank"></i>MavBasic Online Help <i class="fa fa-btn fa-fw fa-question-circle-o"></i></a>
+                    </p>
                 </div>
             </div>
         </div>

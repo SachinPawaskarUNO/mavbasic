907cbbdfedfd84a12de79e4cea40cc938a9c6116
Sachin Pawaskar <spawaskar@unomaha.edu>
2016-12-23 12:08:58 -0600

    Updated User migration - added additional columns

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Carbon\Carbon;
 
 class CreateUsersTable extends Migration
 {
@@ -19,6 +20,15 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->boolean('active')->default(true);
+            $table->string('phone')->nullable();
+            $table->string('default_language')->default('en');
+            $table->string('default_country')->default('US');
+            $table->ipAddress('last_login_ip_address')->nullable();
+            $table->string('last_login_device', 512)->nullable();
+            $table->integer('number_of_logins')->default(0);
+            $table->timestamp('last_login_at')->nullable();
+            $table->timestamp('expiration_at')->default(Carbon::maxValue());
+            $table->timestamp('password_change_at')->nullable();
             $table->string('created_by')->default('System');
             $table->string('updated_by')->default('System');
             $table->rememberToken();

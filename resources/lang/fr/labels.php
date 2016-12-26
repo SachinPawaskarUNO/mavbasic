462dca62f272edfbfe0c277236e26e6ea6c98877
Sachin Pawaskar <spawaskar@unomaha.edu>
2016-12-26 10:09:45 -0600

    localization files to support French

---
+++ resources/lang/fr/labels.php
@@ -0,0 +1,61 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Labels Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for various application specific
+    | labels that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'about' => 'Sur',
+    'address' => 'Adresse',
+    'administration' => 'Administration',
+    'application' => 'Application',
+    'application_name' => 'Nom d\'application',
+    'audit' => 'Vérification',
+    'audit_management' => 'Gestion d\'audit',
+    'best_viewed_by_browser' => 'Meilleure vue par navigateur',
+    'browser_name' => 'Nom du navigateur Web',
+    'browser_version' => 'Version du navigateur Web',
+    'change_password' => 'Changer le mot de passe',
+    'cookies' => 'Cookies du navigateur',
+    'copyright' => 'Copyright',
+    'country' => 'Pays',
+    'email' => 'Courriel',
+    'email_address' => 'Adresse de courriel',
+    'eula' => 'CLUF',
+    'eula_management' => 'Gestion CLUF',
+    'geo_location' => 'Géo localisation',
+    'help' => 'Aidez-moi',
+    'language' => 'Langue',
+    'login' => 'S\'identifier',
+    'logout' => 'Se déconnecter',
+    'name' => 'Nom',
+    'online' => 'En ligne',
+    'online_help' => 'Aide en ligne',
+    'operating_system' => 'Système d\'exploitation',
+    'optimized_for_browser' => 'Optimisé pour le navigateur',
+    'popup_blocker' => 'Bloqueur pop-up',
+    'product' => 'Produit',
+    'product_protection' => 'Protection du produit',
+    'recent_users' => 'Utilisateurs récents',
+    'register' => 'Registre',
+    'role' => 'Rôle',
+    'roles' => 'Les rôles',
+    'setting' => 'Réglage',
+    'settings' => 'Paramètres',
+    'setting_management' => 'Gestion des paramètres',
+    'supported_browsers' => 'Supported Browsers',
+    'support_center' => 'Centre de soutien',
+    'user' => 'Utilisateur',
+    'users' => 'Utilisateurs',
+    'user_management' => 'Gestion des utilisateurs',
+    'user_agent' => 'Agent utilisateur',
+    'version' => 'Version',
+];
---
+++ resources/lang/fr/messages.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Messages Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for various application specific
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'copyright' => 'Copyright &copy; 2012-:yearto Sachin Pawaskar. Tous droits réservés.',
+    'welcome' => 'Bienvenue, :Name',
+    'product_protection' => 'Ce produit est protégé par les lois internationales sur les droits d\'auteurs et par celles des Statuts-Unis.',
+    'optimized_for_browser' => 'Ce produit est optimisé pour Google Chrome 11 et plus. La fonctionnalité peut être compromise si l\'on n\'utilise pas l\'une de ces versions.',
+    'popup_blocker' => 'L\'utilisation d\'un module de blocage des fenêtres intempestives peut nuire au fonctionnement de Zeus. Si vous utilisez un tel module, ajoutez Zeus à la « liste des logiciels autorisés » afin de permettre l\'affichage de ces fenêtres.',
+
+];

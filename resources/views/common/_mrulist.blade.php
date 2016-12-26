d0e44df8b08bf64628686674cd64ab2f7a984ec3
Sachin Pawaskar <spawaskar@unomaha.edu>
2016-12-26 10:08:07 -0600

    Refactoring to support localization

--- resources/views/common/_mrulist.blade.php
+++ resources/views/common/_mrulist.blade.php
@@ -1,6 +1,6 @@
 @if (Auth::user()->getSettingJsonValuesCount($setting) && Auth::user()->getSettingValue($setting) != 0)
     <li class="dropdown-submenu">
-        <a tabindex="-1" href="#"><i class="fa fa-btn fa-fw fa-list-ol"></i>{{$menu}}</a>
+        <a tabindex="-1" href="#"><i class="fa fa-btn fa-fw fa-list-ol"></i>@lang('labels.'.$menu)</a>
         <ul class="dropdown-menu">
             @foreach(Auth::user()->getSettingJsonValuesArray($setting) as $current)
             <li><a href="{{ url('/'.$resource.'/'.$current['id'].'/') }}"><i class="fa fa-btn fa-fw {{ $menuicon }}"></i>{{$current['name']}}</a></li>
--- resources/views/common/nav.blade.php
+++ resources/views/common/nav.blade.php
@@ -28,30 +28,30 @@
                           manage-settings,create-settings,edit-settings,view-settings,delete-settings')
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                        <i class="fa fa-btn fa-fw fa-cogs"></i>Administration<span class="caret"></span></a>
+                        <i class="fa fa-btn fa-fw fa-cogs"></i>@lang('labels.administration')<span class="caret"></span></a>
 
                     <ul class="dropdown-menu multi level" role="menu">
                         @ability('sysadmin,admin', 'manage-users,create-users,edit-users,view-users,delete-users,manage-roles,create-roles,edit-roles,view-roles,delete-roles')
                         <li class="dropdown-submenu">
-                            <a tabindex="-1" href="#"><i class="fa fa-btn fa-fw fa-user-secret"></i>User Management</a>
+                            <a tabindex="-1" href="#"><i class="fa fa-btn fa-fw fa-user-secret"></i>@lang('labels.user_management')</a>
                             <ul class="dropdown-menu">
                                 @ability('admin', 'manage-users,create-users,edit-users,view-users,delete-users')
-                                <li><a href="{{ url('/users') }}"><i class="fa fa-btn fa-fw fa-user"></i>Users</a></li>
+                                <li><a href="{{ url('/users') }}"><i class="fa fa-btn fa-fw fa-user"></i>@lang('labels.users')</a></li>
                                 @endability
                                 @ability('admin', 'manage-roles,create-roles,edit-roles,view-roles,delete-roles')
-                                <li><a href="{{ url('/roles') }}"><i class="fa fa-btn fa-fw fa-users"></i>Roles</a></li>
+                                <li><a href="{{ url('/roles') }}"><i class="fa fa-btn fa-fw fa-users"></i>@lang('labels.roles')</a></li>
                                 @endability
-                                @include ('common._mrulist', ['setting' => 'MRUList_Users', 'menu' => 'Recent Users', 'menuicon' => 'fa-user', 'resource' => 'users'])
+                                @include ('common._mrulist', ['setting' => 'MRUList_Users', 'menu' => 'recent_users', 'menuicon' => 'fa-user', 'resource' => 'users'])
                             </ul>
                         </li>
                         @endability
                         @ability('sysadmin,admin', 'manage-settings,create-settings,edit-settings,view-settings,delete-settings')
-                        <li><a href="{{ url('/settings') }}"><i class="fa fa-btn fa-fw fa-cog"></i>Setting Management</a></li>
-                        <li><a href="{{ url('/eulas') }}"><i class="fa fa-btn fa-fw fa-gavel"></i>EULA Management</a></li>
+                        <li><a href="{{ url('/settings') }}"><i class="fa fa-btn fa-fw fa-cog"></i>@lang('labels.setting_management')</a></li>
+                        <li><a href="{{ url('/eulas') }}"><i class="fa fa-btn fa-fw fa-gavel"></i>@lang('labels.eula_management')</a></li>
                         @endability
                         @ability('sysadmin', '')
                         <li class="divider"></li>
-                        <li><a href="{{ url('/audits') }}"><i class="fa fa-btn fa-fw fa-binoculars"></i>Audit Management</a></li>
+                        <li><a href="{{ url('/audits') }}"><i class="fa fa-btn fa-fw fa-binoculars"></i>@lang('labels.audit_management')</a></li>
                         @endability
 
                         {{--<li class="divider"></li>--}}
@@ -66,8 +66,8 @@
             <ul class="nav navbar-nav navbar-right">
                 <!-- Authentication Links -->
                 @if (Auth::guest())
-                    <li><a href="{{ url('/login') }}"><i class="fa fa-btn fa-fw fa-lg fa-sign-in"></i>Login</a></li>
-                    <li><a href="{{ url('/register') }}">Register</a></li>
+                    <li><a href="{{ url('/login') }}"><i class="fa fa-btn fa-fw fa-lg fa-sign-in"></i>@lang('labels.login')</a></li>
+                    <li><a href="{{ url('/register') }}">@lang('labels.register')</a></li>
                 @else
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
@@ -75,15 +75,15 @@
                         {{-- ToDo: See if we should use this menu structure throughout --}}
                         <ul class="dropdown-menu" role="menu">
                             <li><a href="{{ url('/logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                    <i class="fa fa-btn fa-fw fa-sign-out"></i>Logout
+                                    <i class="fa fa-btn fa-fw fa-sign-out"></i>@lang('labels.logout')
                                 </a>
                                 <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">{{ csrf_field() }}</form>
                             </li>
-                            <li><a href="{{ url('/password/change') }}"><i class="fa fa-btn fa-fw fa-lock"></i>Change Password</a></li>
-                            <li><a href="{{ url('/users/'. Auth::user()->id . '/settings') }}"><i class="fa fa-btn fa-fw fa-cog"></i>Settings</a></li>
+                            <li><a href="{{ url('/password/change') }}"><i class="fa fa-btn fa-fw fa-lock"></i>@lang('labels.change_password')</a></li>
+                            <li><a href="{{ url('/users/'. Auth::user()->id . '/settings') }}"><i class="fa fa-btn fa-fw fa-cog"></i>@lang('labels.settings')</a></li>
                             <li class="divider"></li>
-                            <li><a href="https://SachinPawaskarUNO.github.io/mavbasic" target="_blank"><i class="fa fa-btn fa-fw fa-question-circle-o"></i>Online Help</a></li>
-                            <li><a href="#wizard" data-toggle="modal"><i class="fa fa-btn fa-fw fa-info"></i>About</a></li>
+                            <li><a href="https://SachinPawaskarUNO.github.io/mavbasic" target="_blank"><i class="fa fa-btn fa-fw fa-question-circle-o"></i>@lang('labels.online_help')p</a></li>
+                            <li><a href="#wizard" data-toggle="modal"><i class="fa fa-btn fa-fw fa-info"></i>@lang('labels.about')</a></li>
                         </ul>
                     </li>
                 @endif

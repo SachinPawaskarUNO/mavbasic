eaa7e13a4e5a74bd2977672a37d615cd2e45763d
Sachin Pawaskar <spawaskar@unomaha.edu>
2016-12-25 21:43:52 -0600

    Added language files for labels and messages

---
+++ resources/lang/en/labels.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Labels Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for various application specific
+    | labels that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'address' => 'Address',
+    'application' => 'Application',
+    'application_name' => 'Application Name',
+    'best_viewed_by_browser' => 'Best viewed by browser',
+    'browser_name' => 'Browser Name',
+    'browser_version' => 'Browser Version',
+    'cookies' => 'Cookies',
+    'copyright' => 'Copyright',
+    'country' => 'Country',
+    'email' => 'Email',
+    'email_address' => 'Email Adress',
+    'geo_location' => 'Geo Location',
+    'language' => 'Language',
+    'name' => 'Name',
+    'online' => 'On-line',
+    'operating_system' => 'Operating System',
+    'optimized_for_browser' => 'Optimized for Browser',
+    'popup_blocker' => 'Popup Blocker',
+    'product' => 'Product',
+    'product_protection' => 'Product Protection',
+    'supported_browsers' => 'Supported Browsers',
+    'support_center' => 'Support Center',
+    'user_agent' => 'User Agent',
+    'version' => 'Version',
+
+];

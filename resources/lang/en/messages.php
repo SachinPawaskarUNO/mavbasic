7a01e16dad9f52dd0b546002a8c622db34f8238a
Sachin Pawaskar <spawaskar@unomaha.edu>
2016-12-26 22:00:09 -0600

    moved success and error key-value pairs from labels to messages

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -14,10 +14,23 @@ return [
     */
 
     'copyright' => 'Copyright &copy; 2016-:yearto Sachin Pawaskar. All Rights Reserved.',
-    'welcome' => 'Welcome, :name',
-    'welcome_to_application' => 'Welcome to :appname Application',
-    'product_protection' => 'This product is protected by U.S. and international copyright laws.',
+    'error' => 'Error',
+    'error_edit_user_setting' => 'Error - Updating User Setting :name',
+    'error_eula_accepted' => 'Error - EULA acceptance failed',
     'optimized_for_browser' => 'This product is optimized for Google Chrome browser. Functionality may be compromised if not using one of these versions. The product will work on Internet Explorer 11 & above but functionality may be compromised.',
+    'product_protection' => 'This product is protected by U.S. and international copyright laws.',
     'popup_blocker' => 'Any "popup blockers" could potentially interfere with the use of this application. If using a popup blocker, please be sure to add this application to your blocker\'s "approved list" so that popup windows will be enabled.',
-
+    'success' => 'Success',
+    'success_edit_eula' => 'EULA successfully updated',
+    'success_edit_role' => 'Role successfully updated',
+    'success_edit_user' => 'User successfully updated',
+    'success_eula_accepted' => 'EULA successfully accepted',
+    'success_new_eula' => 'EULA successfully added',
+    'success_new_role' => 'Role successfully added',
+    'success_new_user' => 'User successfully added',
+    'success_edit_user_setting' => 'User Setting successfully updated',
+    'success_edit_user_settings' => 'User Settings successfully updated',
+    'welcome' => 'Welcome',
+    'welcome_to_application' => 'Welcome to :appname Application',
+    'welcome_user' => 'Welcome, :name',
 ];
--- resources/lang/fr/messages.php
+++ resources/lang/fr/messages.php
@@ -14,10 +14,23 @@ return [
     */
 
     'copyright' => 'Copyright &copy; 2012-:yearto Sachin Pawaskar. Tous droits réservés.',
-    'welcome' => 'Bienvenue, :name',
-    'welcome_to_application' => 'Bienvenue à l\'application :appname',
-    'product_protection' => 'Ce produit est protégé par les lois internationales sur les droits d\'auteurs et par celles des Statuts-Unis.',
+    'error' => 'Erreur',
+    'error_edit_user_setting' => 'Erreur - Mise à jour de la configuration de l\'utilisateur :name',
+    'error_eula_accepted' => 'Error - CLUF acceptation a échoué',
     'optimized_for_browser' => 'Ce produit est optimisé pour Google Chrome 11 et plus. La fonctionnalité peut être compromise si l\'on n\'utilise pas l\'une de ces versions.',
     'popup_blocker' => 'L\'utilisation d\'un module de blocage des fenêtres intempestives peut nuire au fonctionnement de Zeus. Si vous utilisez un tel module, ajoutez Zeus à la « liste des logiciels autorisés » afin de permettre l\'affichage de ces fenêtres.',
-
+    'product_protection' => 'Ce produit est protégé par les lois internationales sur les droits d\'auteurs et par celles des Statuts-Unis.',
+    'success' => 'Le succès',
+    'success_edit_eula' => 'CLUF mis à jour avec succès',
+    'success_edit_role' => 'Rôle mis à jour avec succès',
+    'success_edit_user' => 'Utilisateur mis à jour avec succès',
+    'success_eula_accepted' => 'CLUF accepté avec succès',
+    'success_new_eula' => 'CLUF ajouté avec succès',
+    'success_new_role' => 'Rôle ajouté avec succès',
+    'success_new_user' => 'Utilisateur ajouté avec succès',
+    'success_edit_user_setting' => 'Paramètre utilisateur mis à jour avec succès',
+    'success_edit_user_settings' => 'Paramètres utilisateur mis à jour avec succès',
+    'welcome' => 'Bienvenue',
+    'welcome_to_application' => 'Bienvenue à l\'application :appname',
+    'welcome_user' => 'Bienvenue, :name',
 ];

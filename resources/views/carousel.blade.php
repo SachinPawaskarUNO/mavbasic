403a12c33ce899fb86282a7b4dbe76a63d47d95d
Sachin Pawaskar <spawaskar@unomaha.edu>
2016-12-27 10:07:43 -0600

    fixed localization issue

--- resources/views/carousel.blade.php
+++ resources/views/carousel.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading" style="text-align: center;">@lang('messages.welcome', ['name' => $user->name])</div>
+                <div class="panel-heading" style="text-align: center;">@lang('messages.welcome_user', ['name' => $user->name])</div>
 
                 <div class="panel-body">
                     <!-- Carousel

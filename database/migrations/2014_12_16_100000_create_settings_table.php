6c1807cd1cb684a0dc8cec33f51266574a759db3
Sachin Pawaskar <spawaskar@unomaha.edu>
2016-12-28 16:09:08 -0600

    code clean-up

--- database/migrations/2014_12_16_100000_create_settings_table.php
+++ database/migrations/2014_12_16_100000_create_settings_table.php
@@ -30,7 +30,6 @@ class CreateSettingsTable extends Migration
 
         // Create table for associating roles to users (Many-to-Many)
         Schema::create('setting_user', function (Blueprint $table) {
-//            $table->increments('id');
             $table->integer('user_id')->unsigned();
             $table->integer('setting_id')->unsigned();
             $table->string('value');
